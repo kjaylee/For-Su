4f2b25cb4511efda2189551044d315b6e2e5deb6
Jay Lee <k.jaylee@gmail.com>
2021-03-11 13:18:35 +0900

    CHORE: remove git's pods.

--- Pods/Target Support Files/Pods-for su-for suUITests/Pods-for su-for suUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_for_su_for_suUITests : NSObject
-@end
-@implementation PodsDummy_Pods_for_su_for_suUITests
-@end
--- Pods/Target Support Files/Pods-for su/Pods-for su-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_for_su : NSObject
-@end
-@implementation PodsDummy_Pods_for_su
-@end
